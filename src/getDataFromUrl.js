a7ab5bc49af4df4b69501113f9c3c107c19e6379
Dmitry Klimenko <dedooman@mail.ru>
2021-05-10 09:04:35 +0500

    test

--- src/getDataFromUrl.js
+++ src/getDataFromUrl.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 export default (url, validation) => {
   validation.then((statusValid) => {
     if (statusValid) {
-      axios.get(url)
+      axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}`)
         .then((data) => {
           const document = parser(data);
           watcherState.dataRss.push({ idFeed: `feed${watcherState.dataRss.length + 1}`, document });
