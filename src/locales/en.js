dcf6ccdd4e3c917fc6ba6b97b93a22eee0ff903c
Dmitry Klimenko <dedooman@mail.ru>
2021-06-17 10:37:41 +0500

    refactoring

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,21 +1,21 @@
 export default {
   translation: {
+    modal: {
+      close: 'Close',
+      go: 'Go',
+    },
     error: {
       alreadyExist: 'RSS already exist',
       invalidUrl: 'The link must be a valid URL',
       invalidRss: 'The resource does not contain a valid RSS',
       network: 'Network Error',
     },
-    modal: {
-      close: 'Close',
-      go: 'Go',
-    },
-    post: {
-      preview: 'Viewing',
-    },
     formStatus: {
       success: 'RSS uploaded successfully',
       wait: 'Viewing',
+    },
+    post: {
+      preview: 'Viewing',
     },
     feedsTitle: 'Feeds',
     postsTitle: 'Posts',
